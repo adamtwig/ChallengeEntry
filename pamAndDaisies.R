3be72ed9e5fbbd3f095fc51dc796256ff90d7b94
InnoCentiveOpenNEXChallenge <terwilla@mail.gvsu.edu>
2014-10-21 08:32:57 -0400

    Update pamAndDaisies.R
    Added documentation and write.table output line

--- pamAndDaisies.R
+++ pamAndDaisies.R
@@ -27,18 +27,23 @@ row.names(projectionData) <- rawProjections[,1]
 install.packages('cluster')
 library(cluster)
 
+# find dissimilarity matrix
 daisyOut <- as.matrix(daisy(projectionData))
 
+# cities shouldn't pair with themselves in different years
 diag(daisyOut[1:22,23:44]) <- rep(999999, times=22)
-
 diag(daisyOut[23:44,1:22]) <- rep(999999, times=22)
 
+# cities shoudn't pair with cities from the same years
 daisyOut[1:22,1:22] <- matrix(999999, 22, 22)
-
 daisyOut[23:44, 23:44] <- matrix(999999, 22, 22)
 
-View(daisyOut)
-
+# run clustering for 22 clusters (44 cities / 2 pairs each = 22 clusters)
+# use the dissimilarity matrix for clustering
 pamOut <- pam(daisyOut, 22, diss=TRUE)
 
+# view the clusters
 sort(pamOut$clustering)
+
+# input own directory and run if desired to export output to text file
+# write.table(sort(pamOut$clustering), ... (chosen directory) ex. "/usr/local/src.padOut.txt", sep="\t"
